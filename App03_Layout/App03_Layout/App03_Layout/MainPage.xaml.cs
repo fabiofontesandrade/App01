345a9c204ea086a3384ee610d2af49b4d4c52bce
Fabio Fontes <fabiofontesandrade@outlook.com>
2019-11-29 12:09:28 -0300

    app 03

---
+++ App03_Layout/App03_Layout/App03_Layout/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App03_Layout
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPageStack(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layout.Stack.StackPage());
+        }
+        private void GoPageGrid(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layout.Grid.GridPage());
+        }
+        private void GoPageAbsolute(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layout.Absolute.AbsolutePage());
+        }
+        private void GoPageRelative(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layout.Relative.RelativePage());
+        }
+        private void GoPageScroll(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layout.Scroll.ScrollPage());
+        }
+    }
+}
